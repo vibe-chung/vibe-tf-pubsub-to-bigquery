22fa681007a164a8ecc991eda4a53c447b7a39a4
chung <git@chungy.org>
2025-10-12 00:22:49 +0100

    Refactor: move variable definitions to variables.tf, add .terraform-version and example tfvars file

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "project_id" {
+    description = "The GCP project ID"
+    type        = string
+}
+
+variable "region" {
+    description = "The GCP region"
+    type        = string
+    default     = "us-west1"
+}
+
+variable "pubsub_topic" {
+    description = "The name of the Pub/Sub topic"
+    type        = string
+}
+
+variable "bigquery_dataset" {
+    description = "The name of the BigQuery dataset"
+    type        = string
+}
+
+variable "bigquery_table" {
+    description = "The name of the BigQuery table"
+    type        = string
+}
